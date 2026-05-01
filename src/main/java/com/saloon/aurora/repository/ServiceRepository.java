e331ebe9b0829c1686e5eed614a578ac18047f7b
IT25102042 <it25102042@my.sliit.lk>
2026-05-01 23:49:29 +0530

    Service: Controller,Dto,Repository,Service, ServiceImpl and serviceDataLoad.js, made uncomment on ReviewEntity

---
+++ src/main/java/com/saloon/aurora/repository/ServiceRepository.java
@@ -0,0 +1,10 @@
+package com.saloon.aurora.repository;
+
+import com.saloon.aurora.entity.ServiceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceRepository extends JpaRepository<ServiceEntity, Integer> {
+
+}

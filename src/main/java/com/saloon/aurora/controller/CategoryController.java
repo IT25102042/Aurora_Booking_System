04ef6a6dfcfcd5e4b6437809b27509227a075b44
IT25102042 <it25102042@my.sliit.lk>
2026-05-02 15:24:38 +0530

    Category Data Insert to Database with Validation done

--- src/main/java/com/saloon/aurora/controller/CategoryController.java
+++ src/main/java/com/saloon/aurora/controller/CategoryController.java
@@ -3,11 +3,10 @@ package com.saloon.aurora.controller;
 import com.saloon.aurora.dto.CategoryDto;
 import com.saloon.aurora.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +22,19 @@ public class CategoryController {
     public ResponseEntity<List<CategoryDto>> getAllCategories() {
         List<CategoryDto> categories = categoryService.getAllCategories();
         return ResponseEntity.ok(categories);
+    }
+
+    @PostMapping("/register-category")
+    public ResponseEntity<String> registerCategory(@Valid @RequestBody CategoryDto categoryDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.getFieldError().getDefaultMessage());
+        }
+        try {
+            categoryService.registerCategory(categoryDto);
+            return ResponseEntity.ok("Category saved successfully");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }

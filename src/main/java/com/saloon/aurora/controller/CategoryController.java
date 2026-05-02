42d619bdb352539d9cc378038e41b35189618c45
IT25102042 <it25102042@my.sliit.lk>
2026-05-02 14:26:24 +0530

    Category Data Load to table done

---
+++ src/main/java/com/saloon/aurora/controller/CategoryController.java
@@ -0,0 +1,28 @@
+package com.saloon.aurora.controller;
+
+import com.saloon.aurora.dto.CategoryDto;
+import com.saloon.aurora.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/category")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<CategoryDto>> getAllCategories() {
+        List<CategoryDto> categories = categoryService.getAllCategories();
+        return ResponseEntity.ok(categories);
+    }
+
+}

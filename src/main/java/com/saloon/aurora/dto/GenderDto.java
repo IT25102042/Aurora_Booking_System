c2617ec5d03f9a094212e2d761bd5c10ef3eafa5
IT25102042 <it25102042@my.sliit.lk>
2026-05-02 21:45:54 +0530

    loaded the Category Gender Status dropdowns in Add Service Modal in Service Management of adminPanel.html

---
+++ src/main/java/com/saloon/aurora/dto/GenderDto.java
@@ -0,0 +1,13 @@
+package com.saloon.aurora.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GenderDto {
+    private Integer id;
+    private String gender;
+}

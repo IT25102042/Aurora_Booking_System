9da4e9585314cc4bd2dc4e91e6a679d52c445d19
BHNKCG <it25102044@my.sliit.lk>
2026-05-02 12:14:50 +0530

    Made a change PaymentMethodEntity and PaymentMethodRepository uploaded

--- src/main/java/com/saloon/aurora/entity/PaymentMethodEntity.java
+++ src/main/java/com/saloon/aurora/entity/PaymentMethodEntity.java
@@ -11,11 +11,10 @@ import lombok.NoArgsConstructor;
 @Table(name = "payment_methods")
 public class PaymentMethodEntity {
 
-    @ManyToOne
-    @JoinColumn(name = "payment_method_id", nullable = false)
-    private PaymentMethodEntity paymentMethod;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "payment_method", nullable = false, length = 45)
+    private String paymentMethod;
 }

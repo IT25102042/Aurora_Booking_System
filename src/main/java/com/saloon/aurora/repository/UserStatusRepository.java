79519234455c8962b83141bfff003d95346d0ee5
Kavindya <it25102041@my.sliit.lk>
2026-05-10 13:31:21 +0530

    To Resolve the Github Conflict

--- src/main/java/com/saloon/aurora/repository/UserStatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.saloon.aurora.repository;
-
-import com.saloon.aurora.entity.UserStatusEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserStatusRepository extends JpaRepository<UserStatusEntity, Integer> {
-}
--- src/main/java/com/saloon/aurora/repository/UserTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.saloon.aurora.repository;
-
-import com.saloon.aurora.entity.UserTypeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserTypeRepository extends JpaRepository<UserTypeEntity, Integer> {
-}

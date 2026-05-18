c7244eb92bddd2ea000a49a5d89cbc3a4352713a
IT25102042 <it25102042@my.sliit.lk>
2026-05-19 00:37:55 +0530

    added missing UserRepository code

--- src/main/java/com/saloon/aurora/repository/UserRepository.java
+++ src/main/java/com/saloon/aurora/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.saloon.aurora.repository;
 
-public interface UserRepository {
+import com.saloon.aurora.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 }
